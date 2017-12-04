6938a8442b5d5ea920e1f72b59085b714cded49e
Darren Evans <darrenrevans10@gmail.com>
2017-12-03 19:28:24 -0500

    updated tweets

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'qJw4StqbrQSpqx9QBxJKNLVuE',
+  consumer_secret: 'NoJ9m01aGaihAKYQg6jEpY55GHOgHI4TegiII6VnJu4GCegXnQ',
+  access_token_key: '937453993068056576-7jPBtRhnqExLYhSwq2FONgEQbqRyfq8',
+  access_token_secret: 'LD3w3MhcuIfgZHxkfpfFMmtfVsGxBTuhYMP3fT7WziOHe'
 }
 
 module.exports = twitterKeys;
--- liriBot.js
+++ liriBot.js
@@ -1,3 +1,27 @@
 var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 var request = require("request");
+var keys = require("./keys.js");
+var input = process.argv[2];
+
+var client = new twitter(keys);
+var params = {screen_name: 'LiriDeveloper'};
+
+switch(input) {
+	case "my-tweets":
+		myTweets();
+		break;
+	case "spotify-this-song":
+		mySpotify();
+		break;
+}
+
+function myTweets() {
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    for (var i = 0; i < tweets.length; i++) {
+	    	console.log("Tweet: " + tweets[i].text);
+	    }
+	  }
+	});
+};
